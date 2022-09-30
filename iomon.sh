2ab76d2abcb6dcb21e39684d065c06e9ea290b5b
kinghow <18002408+kinghows@users.noreply.github.com>
2022-09-30 11:10:24 +0800

    Update iomon.sh

--- iomon.sh
+++ iomon.sh
@@ -6,11 +6,13 @@ LOGDIR=/root/monitor/log/
 [ -d $LOGDIR ] || mkdir -p $LOGDIR
 
 SUFFIX=`date +%Y%m%d`
+CTIME=`date +%H:%M:%S`
 INTV=10
 CNT=8640
+export S_TIME_FORMAT='ISO'
 
 /usr/bin/dstat -cmdnylst $INTV $CNT >> $LOGDIR/dstat_$SUFFIX &
-/usr/bin/iostat -mdx $INTV $CNT |awk '/^sd*|^dm*/'|awk -v CTIME=$CTIME '{ print $0,CTIME}' >> $LOGDIR/iostat_$SUFFIX &
+/usr/bin/iostat -mdxt $INTV $CNT |awk '/+0800$|^dm-[456]/' >> $LOGDIR/iostat_$SUFFIX &
 
 find $LOGDIR -mtime +30 -name "dstat_*" -exec rm -rf {} \;
 find $LOGDIR -mtime +30 -name "iostat_*" -exec rm -rf {} \;
